2789cb15d5fd79b9d8cbb07440de7a6149ae25e8
camper3d <camper3d@gmail.com>
2025-11-29 11:17:14 +0300

    feat: модель встреч

---
+++ app/models/meeting.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models.base import Base
+from sqlalchemy import Table
+
+
+meeting_participants = Table(
+    "meeting_participants",
+    Base.metadata,
+    Column("meeting_id", Integer, ForeignKey("meetings.id")),
+    Column("user_id", Integer, ForeignKey("users.id")),
+)
+
+
+class Meeting(Base):
+    __tablename__ = "meetings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=False)
+
+    creator_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    creator = relationship("User", foreign_keys=[creator_id])
+
+    participants = relationship("User", secondary=meeting_participants, back_populates="meetings")
