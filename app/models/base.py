a1717136cc7849fd471f24cac8ccc7385d0519a6
camper3d <camper3d@gmail.com>
2025-11-29 10:00:30 +0300

    feat: добавлена асинхронная настройка SQLAlchemy

---
+++ app/models/base.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+Base = declarative_base()
