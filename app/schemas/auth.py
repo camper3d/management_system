4e0defc420558c0b28ad36b1e312317f69764425
camper3d <camper3d@gmail.com>
2025-11-30 11:47:21 +0300

    feat: схемы юзера и токена

---
+++ app/schemas/auth.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    full_name: str | None = None
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    user_id: int | None = None
