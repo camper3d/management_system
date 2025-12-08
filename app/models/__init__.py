1889ce72ac2cc41b572c9459313b1bdaa43acb46
camper3d <camper3d@gmail.com>
2025-12-08 09:29:57 +0300

    feat: добавлены импорты всех моделей

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .base import Base
+from .user import User
+from .team import Team
+from .task import Task
+from .meeting import Meeting
+from .evaluation import Evaluation
+from .comment import Comment
