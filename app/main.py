68a70901711dd308f4c216004ecaa0d8947d7dd9
camper3d <camper3d@gmail.com>
2025-12-08 11:42:00 +0300

    feat: добавлен task_router

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 from app.models.base import Base, engine
 from app.api.auth import router as auth_router
 from app.api.team import router as team_router
-
+from app.api.task import router as task_router
 
 app = FastAPI(title="MVP")
 
 
 app.include_router(auth_router, prefix="/api")
 app.include_router(team_router, prefix="/api")
+
+
+app.include_router(task_router, prefix="/api")
 
 
 @app.get("/")
