c70625e4e4729d6d9ecfcc8ff3da186469794dc7
camper3d <camper3d@gmail.com>
2025-12-12 13:42:53 +0300

    feat: добавлен eval_router

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ from app.models.base import Base, engine
 from app.api.auth import router as auth_router
 from app.api.team import router as team_router
 from app.api.task import router as task_router
+from app.api.evaluation import router as eval_router
 
 app = FastAPI(title="MVP")
 
 
 app.include_router(auth_router, prefix="/api")
 app.include_router(team_router, prefix="/api")
-
-
 app.include_router(task_router, prefix="/api")
+app.include_router(eval_router, prefix="/api")
 
 
 @app.get("/")
