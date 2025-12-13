65988401a116889d0cb5080f6b9ad2b7004e6bac
camper3d <camper3d@gmail.com>
2025-12-13 17:04:55 +0300

    feat: добавлен синхронный движок для админки

--- backend/models/base.py
+++ backend/models/base.py
@@ -1,12 +1,16 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.core.config import settings
+from backend.core.config import settings
+from sqlalchemy import create_engine
 
 DATABASE_URL = settings.DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-
+sync_engine = create_engine(
+    settings.DATABASE_URL.replace("sqlite+aiosqlite", "sqlite").replace("postgresql+asyncpg", "postgresql"),
+    echo=False,
+)
 
 AsyncSessionLocal = sessionmaker(
     bind=engine,
