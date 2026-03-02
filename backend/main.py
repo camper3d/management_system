0fdead09036aa4f887a60b9c3919f530643293e1
camper3d <camper3d@gmail.com>
2026-03-02 10:49:41 +0300

    feat: добавлены настройки шаблонов и статических файлов

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from backend.models.base import Base, engine
 from backend.api import auth, team, task, evaluation, meeting, event_calendar
 from backend.core.config import settings
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+static_dir = os.path.join(BASE_DIR, "static")
 
 
 app = FastAPI(title="MVP")
 
+
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+
+app.state.templates = templates
 
 app.include_router(auth.router, prefix="/api")
 app.include_router(team.router, prefix="/api")
