62086890ab090586659e035b7ea8e911c6695644
camper3d <camper3d@gmail.com>
2026-03-06 10:56:18 +0300

    feat: добавлены AuthMiddleware,auth_html_router,html_router,auth_api_router

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,32 @@
 from fastapi import FastAPI, Request
 from backend.models.base import Base, engine
 from backend.api import auth, team, task, evaluation, meeting, event_calendar
-from backend.core.config import settings
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import os
+from backend.middleware.auth_middleware import AuthMiddleware
+from backend.api.html_views import html_router
+from backend.api.auth import auth_api_router, auth_html_router
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
-static_dir = os.path.join(BASE_DIR, "static")
 
 
 app = FastAPI(title="MVP")
 
+app.state.templates = templates
 
+static_dir = os.path.join(BASE_DIR, "static")
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 
-app.state.templates = templates
+app.add_middleware(AuthMiddleware)
+app.include_router(auth_html_router)
+app.include_router(html_router)
 
-app.include_router(auth.router, prefix="/api")
+# app.include_router(auth.router, prefix="/api")
+app.include_router(auth_api_router, prefix="/api")
 app.include_router(team.router, prefix="/api")
 app.include_router(task.router, prefix="/api")
 app.include_router(evaluation.router, prefix="/api")
